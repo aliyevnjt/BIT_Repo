8fade70db12bee2e4c22b3e8592808a9f3cb3928
Nijat Aliyev <nijataliyev@Nijats-MacBook-Pro.local>
2019-04-25 20:11:14 -0400

    first commit

---
+++ test/src/test/java/firstmaven/TestMaven.java
@@ -0,0 +1,31 @@
+package firstmaven;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestMaven {
+	
+	WebDriver driver;
+	
+	@Before
+	public void setUp() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+	}
+	
+	@Test
+	public void test1() {
+		driver.get("http://newtours.demoaut.com/");
+	}
+	
+	@After
+	public void tearDown() {
+		driver.quit();
+	}
+}
