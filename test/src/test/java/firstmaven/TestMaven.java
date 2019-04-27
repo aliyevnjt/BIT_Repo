8d20b66689b9a2968e7bd2bc698bb73b5c61fa92
Kamran Ayyubov <diakamran@gmail.com>
2019-04-27 09:37:16 -0400

    kamrans change2

--- test/src/test/java/firstmaven/TestMaven.java
+++ test/src/test/java/firstmaven/TestMaven.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package firstmaven;
 
 import org.junit.After;
@@ -7,65 +7,28 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestMaven {
-	
+
 	WebDriver driver;
-	
+
 	@Before
 	public void setUp() {
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 	}
-	
+
 	@Test
 	public void test1() {
 		driver.get("http://newtours.demoaut.com/");
 
 		// comment from Kamran's laptop
-		
+
 	}
-	
+
 	@After
 	public void tearDown() {
 		driver.quit();
 	}
 }
-=======
-package firstmaven;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
-
-public class TestMaven {
-
-	WebDriver driver;
-
-	@Before
-	public void setUp() {
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
-	}
-
-	@Test
-	public void test1() {
-		driver.get("http://newtours.demoaut.com/");
-		
-		// steps in command: git add -A, git commit -m "dsd", git push
-		
-		// A useful link : http://rogerdudler.github.io/git-guide/
-	}
-
-	@After
-	public void tearDown() {
-//		driver.quit();
-	}
-}
->>>>>>> 5628b52804891c5dd9435b2a3895a798a016fb33
