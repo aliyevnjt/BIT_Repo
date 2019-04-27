6b9131c810ff77a21d5fe0704fc72e4f92b2146a
Gozal Huseynova <gozelhuseynova@gmail.com>
2019-04-27 11:24:50 -0400

    ghsdfj

--- test/src/test/java/firstmaven/TestMaven.java
+++ test/src/test/java/firstmaven/TestMaven.java
@@ -2,6 +2,8 @@ package firstmaven;
 
 import java.util.concurrent.TimeUnit;
 
+import java.util.concurrent.TimeUnit;
+
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -15,6 +17,7 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
 
 public class TestMaven {
 
@@ -30,30 +33,41 @@ public class TestMaven {
 
 	@Test
 	public void test1() {
-		
+
+
 		Assert.assertTrue(driver.getTitle().contains("New Cars"));
-		WebElement selecAstockType = driver.findElement(By.name("stockType"));
-		selectBytext(selecAstockType, "New Cars");
+		WebElement selectAstockType = driver.findElement(By.name("stockType"));
+		selectBytext(selectAstockType, "New Cars");
 		WebElement make = driver.findElement(By.name("makeId"));
 		selectBytext(make, "Audi");
-		WebElement model = driver.findElement(By.name("modelId"));
-		selectBytext(model, "S5");
-		WebDriverWait wait = new WebDriverWait(driver, 10);
-		WebElement search = driver.findElement(By.className("NZE2g"));
+		WebElement modelId = driver.findElement(By.name("modelId"));
+		selectBytext(modelId, "S5");
+		WebDriverWait wait =  new WebDriverWait(driver, 10);
+		WebElement search =driver.findElement(By.className("NZE2g"));
 		wait.until(ExpectedConditions.elementToBeClickable(search));
 		search.click();
 		String header = driver.findElement(By.className("srp-header")).getText();
 		Assert.assertEquals("New Audi S5 for Sale", header);
-	}
+		
+//		select.selectByVisibleText("New Cars");
+//		WebElement make= driver.findElement(By.name("makeId"));
+//        select = new Select(make);
+//        select.selectByVisibleText("Audi");
+//        WebElement modelId= driver.findElement(By.name("modelId"));
+//        select = new Select(modelId);
+//        select.selectByVisibleText("S5");
+//        
 
+		
+	}
+	
 	public static void selectBytext(WebElement element, String visibleText) {
-		Select select = new Select(element);
-		select.selectByVisibleText(visibleText); 
+		Select select =  new Select(element);
+		select.selectByVisibleText(visibleText);
 	}
-
 	@After
 	public void tearDown() {
-		driver.quit();
+		//driver.quit();
 	}
 }
 
