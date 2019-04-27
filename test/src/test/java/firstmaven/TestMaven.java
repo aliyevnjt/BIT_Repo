36f32102e2709698e6f54e17aacdccda9f4784cc
Nijat Aliyev <nijataliyev@Nijats-MacBook-Pro.local>
2019-04-27 09:39:44 -0400

    Resolved conflicts

--- test/src/test/java/firstmaven/TestMaven.java
+++ test/src/test/java/firstmaven/TestMaven.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package firstmaven;
 
 import org.junit.After;
@@ -24,8 +23,15 @@ public class TestMaven {
 	public void test1() {
 		driver.get("http://newtours.demoaut.com/");
 
+
 		// comment from Kamran's laptop
 		
+
+		// some comment .....
+		// change from Fehmi
+		// Nijat
+		// New line
+
 	}
 	
 	@After
@@ -33,39 +39,3 @@ public class TestMaven {
 		driver.quit();
 	}
 }
-=======
-package firstmaven;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
-
-public class TestMaven {
-
-	WebDriver driver;
-
-	@Before
-	public void setUp() {
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
-	}
-
-	@Test
-	public void test1() {
-		driver.get("http://newtours.demoaut.com/");
-		
-		// steps in command: git add -A, git commit -m "dsd", git push
-		
-		// A useful link : http://rogerdudler.github.io/git-guide/
-	}
-
-	@After
-	public void tearDown() {
-//		driver.quit();
-	}
-}
->>>>>>> 5628b52804891c5dd9435b2a3895a798a016fb33
