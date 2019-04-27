50bac5b991e4ed7a490d1eadbbec62bf0b9ca5ec
mehmet <saidsayar@gmail.com>
2019-04-27 16:45:04 -0400

    Just a test

---
+++ test/src/test/java/firstmaven/TestClass.java
@@ -0,0 +1,11 @@
+package firstmaven;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is just a test. Please remain seated. And keep walking through the wonders of Maven");
+
+	}
+
+}
