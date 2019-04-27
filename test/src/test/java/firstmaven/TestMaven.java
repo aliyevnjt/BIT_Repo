4cea59accc348d326d7130a4e12074e9c9ad1d2a
mehmet <saidsayar@gmail.com>
2019-04-27 19:23:30 -0400

    Test

--- test/src/test/java/firstmaven/TestMaven.java
+++ test/src/test/java/firstmaven/TestMaven.java
@@ -13,11 +13,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-<<<<<<< Updated upstream
-=======
-import org.testng.Assert;
-
->>>>>>> Stashed changes
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestMaven {
@@ -28,36 +23,22 @@ public class TestMaven {
 	public void setUp() {
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
-<<<<<<< Updated upstream
 
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-		driver.get("https://www.cars.com/");
-	
-=======
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.get("https://www.cars.com/");
->>>>>>> Stashed changes
+
 	}
 	
-//	aman Allahim
-//	eger bu yaziyi okuyorsaniz bilin ki ben coktaan cok uzak diyarlardan git push yapmisim demektir.
-//	
-//	FFFFFF  EEEEEE	H 	 H	M	  M	 I			
-//	F	    E		H	 H	MM   MM	 I
-//	FFFFFF  EEEE	HHHHHH	M M M M	 I		FEHMI EVDE SIGARA ICME, TERBIYESIZ FEHMI
-//	F	    E		H	 H	M  M  M	 I
-//	F		EEEEEE	H	 H	M	  M	 I
 
 	@Test
 	public void test1() {
 
-		Assert.assertTrue(driver.getTitle().contains("New Cars")); // assert that we are on the correct website.
+		assertTrue(driver.getTitle().contains("New Cars")); // assert that we are on the correct website.
 
 		WebElement selectAStockType = driver.findElement(By.name("stockType"));
 		selectByText(selectAStockType, "New Cars");
 
-<<<<<<< Updated upstream
+
 		assertTrue(driver.getTitle().contains("New Cars"));
 
 		System.out.println(driver.getTitle());
@@ -79,23 +60,6 @@ public class TestMaven {
 		String actualHeader = driver.findElement(By.xpath("//h1[@class='srp-header']")).getText();
 
 		assertEquals(expectedHeader, actualHeader);
-=======
-		WebElement makeOfTheCar = driver.findElement(By.name("makeId"));
-		selectByText(makeOfTheCar, "Audi");
-
-		WebElement model = driver.findElement(By.name("modelId"));
-		selectByText(model, "S5");
-
-		WebDriverWait wait = new WebDriverWait (driver, 10); //explicit wait
-		WebElement search = driver.findElement(By.className("NZE2g"));
-		wait.until(ExpectedConditions.elementToBeClickable(search));
-		
-		
-		driver.findElement(By.className("NZE2g")).click();
-		// New Audi S5 for Sale
-		// srp-header
-		String header = driver.findElement(By.className("srp-header")).getText();
-		Assert.assertEquals("New Audi S5 for Sale", header);
 
 	}
 
@@ -103,25 +67,16 @@ public class TestMaven {
 
 		Select sel = new Select(element);
 		sel.selectByVisibleText(visibletext);
->>>>>>> Stashed changes
 
-	}
-
-	public static void selectByText(WebElement element, String visibleText) {
-		Select select = new Select(element);
-		select.selectByVisibleText(visibleText);
 	}
 	@After
 	public void tearDown() {
-<<<<<<< Updated upstream
+
 
 
 //		driver.quit();
 
 
 
-=======
-//		driver.quit();
->>>>>>> Stashed changes
 	}
 }
