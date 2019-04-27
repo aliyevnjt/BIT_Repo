4c31808857e9e04025e4513e6c7701b48d0004fe
Nijat Aliyev <nijataliyev@Nijats-MacBook-Pro.local>
2019-04-27 11:16:01 -0400

    test 1 was added

--- test/src/test/java/firstmaven/TestMaven.java
+++ test/src/test/java/firstmaven/TestMaven.java
@@ -1,10 +1,18 @@
 package firstmaven;
 
+import java.util.concurrent.TimeUnit;
+
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -16,25 +24,32 @@ public class TestMaven {
 	public void setUp() {
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("https://www.cars.com/");
 	}
 
 	@Test
 	public void test1() {
-		driver.get("http://newtours.demoaut.com/");
-
-
 		
-
-		// some comment .....
-		// change from Fehmi
-		// Nijat
-		// New line
-		//Cenab qizil Shlyager
-		
+		Assert.assertTrue(driver.getTitle().contains("New Cars"));
+		WebElement selecAstockType = driver.findElement(By.name("stockType"));
+		selectBytext(selecAstockType, "New Cars");
+		WebElement make = driver.findElement(By.name("makeId"));
+		selectBytext(make, "Audi");
+		WebElement model = driver.findElement(By.name("modelId"));
+		selectBytext(model, "S5");
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		WebElement search = driver.findElement(By.className("NZE2g"));
+		wait.until(ExpectedConditions.elementToBeClickable(search));
+		search.click();
+		String header = driver.findElement(By.className("srp-header")).getText();
+		Assert.assertEquals("New Audi S5 for Sale", header);
 	}
 
-	// comment from Kamran's laptop
-	// Khazar
+	public static void selectBytext(WebElement element, String visibleText) {
+		Select select = new Select(element);
+		select.selectByVisibleText(visibleText); 
+	}
 
 	@After
 	public void tearDown() {
