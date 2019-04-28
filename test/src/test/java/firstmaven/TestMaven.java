791b02eb2f8224290c9c64e9aae96a300b177824
Azer Safar <azersfr@gmail.com>
2019-04-28 10:02:34 -0400

    changes by 10:02 am

--- test/src/test/java/firstmaven/TestMaven.java
+++ test/src/test/java/firstmaven/TestMaven.java
@@ -48,7 +48,7 @@ public class TestMaven {
 		String expectedHeader = "New Audi S5 for Sale";
 		String actualHeader = driver.findElement(By.xpath("//h1[@class='srp-header']")).getText();
 		Assert.assertEquals(expectedHeader, actualHeader);
-		// Changes in branch DEV// changes by me
+		// Changes in branch DEV // changes by me
 	}
 
 	public static void selectByText(WebElement element, String visibleText) {
